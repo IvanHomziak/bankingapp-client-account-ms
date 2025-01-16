3a1a89669b60b37eb8d6c0a0ce33b8cf396c35f1
ivanhomziak <ivan.homziak@gmail.com>
2025-01-17 00:20:17 +0100

    EnableDiscoveryClient

--- src/main/java/com/ihomziak/clientmanagerservice/ClientManagerService.java
+++ src/main/java/com/ihomziak/clientmanagerservice/ClientManagerService.java
@@ -2,8 +2,10 @@ package com.ihomziak.clientmanagerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClientManagerService {
 
 	public static void main(String[] args) {
