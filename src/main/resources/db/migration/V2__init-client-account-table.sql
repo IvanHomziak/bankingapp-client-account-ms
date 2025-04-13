dadfb6c761dac8ed5fd7538ee24231d8e50027cf
ihomziak <ivan.homziak@onedatascan.com>
2025-04-13 17:41:14 +0200

    ref

--- src/main/resources/db/migration/V2__init-client-account-table.sql
+++ src/main/resources/db/migration/V2__init-client-account-table.sql
@@ -18,3 +18,17 @@ CREATE TABLE client
     updated_at    TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     uuid          VARCHAR(255) NOT NULL UNIQUE
 );
+
+-- Create the account table with a unique constraint on account_number
+CREATE TABLE account
+(
+    account_id     BIGINT(20) AUTO_INCREMENT PRIMARY KEY,
+    account_number VARCHAR(255) NOT NULL UNIQUE,
+    account_type   VARCHAR(255),
+    balance        FLOAT,
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_update    TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    client_id      BIGINT(20),
+    uuid           VARCHAR(255) NOT NULL UNIQUE,
+    CONSTRAINT fk_client FOREIGN KEY (client_id) REFERENCES client (client_id)
+);
