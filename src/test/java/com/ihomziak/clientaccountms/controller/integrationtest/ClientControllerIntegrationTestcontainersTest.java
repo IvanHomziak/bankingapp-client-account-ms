899a3b2ef85f693f00a1c4c6b51329ee9cbc9a02
ihomziak <ivan.homziak@onedatascan.com>
2025-06-22 13:49:49 +0200

    added integration test ClientControllerIntegrationTestcontainersTest.class - testDeleteClient_whenValidDetailsProvided_returnsClientDetails

--- src/test/java/com/ihomziak/clientaccountms/controller/integrationtest/ClientControllerIntegrationTestcontainersTest.java
+++ src/test/java/com/ihomziak/clientaccountms/controller/integrationtest/ClientControllerIntegrationTestcontainersTest.java
@@ -2,6 +2,7 @@ package com.ihomziak.clientaccountms.controller.integrationtest;
 
 import static com.ihomziak.clientaccountms.util.constants.Endpoints.ClientEndpoints.ADD_CLIENT;
 import static com.ihomziak.clientaccountms.util.constants.Endpoints.ClientEndpoints.API_CLIENT_V1;
+import static com.ihomziak.clientaccountms.util.constants.Endpoints.ClientEndpoints.DELETE_CLIENT;
 
 import java.util.Arrays;
 
@@ -19,6 +20,7 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -109,6 +111,82 @@ public class ClientControllerIntegrationTestcontainersTest {
 			softly.assertThat(createdUserDetailsEntity.getStatusCode())
 				.as("Response status should be CREATED")
 				.isEqualTo(HttpStatus.CREATED);
+
+			softly.assertThat(createdUserDetails.getUUID())
+				.as("Created user UUID should not be null")
+				.isNotNull();
+
+			softly.assertThat(createdUserDetails.getFirstName())
+				.as("First name should match")
+				.isEqualTo(userDetailsRequestObject.get("firstName"));
+
+			softly.assertThat(createdUserDetails.getLastName())
+				.as("Last name should match")
+				.isEqualTo(userDetailsRequestObject.get("lastName"));
+
+			softly.assertThat(createdUserDetails.getDateOfBirth())
+				.as("Date of birth should match")
+				.isEqualTo(userDetailsRequestObject.get("dateOfBirth"));
+
+			softly.assertThat(createdUserDetails.getTaxNumber())
+				.as("Tax number should match")
+				.isEqualTo(userDetailsRequestObject.get("taxNumber"));
+
+			softly.assertThat(createdUserDetails.getEmail())
+				.as("Email should match")
+				.isEqualTo(userDetailsRequestObject.get("email"));
+
+			softly.assertThat(createdUserDetails.getPhoneNumber())
+				.as("Phone number should match")
+				.isEqualTo(userDetailsRequestObject.get("phoneNumber"));
+
+			softly.assertThat(createdUserDetails.getAddress())
+				.as("Address should match")
+				.isEqualTo(userDetailsRequestObject.get("address"));
+		}
+	}
+
+	@Test
+	@DisplayName("Created user can be deleted")
+	public void testDeleteClient_whenValidDetailsProvided_returnsClientDetails() throws JSONException {
+		// Arrange
+		JSONObject userDetailsRequestObject = new JSONObject();
+		userDetailsRequestObject.put("firstName", faker.name().firstName());
+		userDetailsRequestObject.put("lastName", faker.name().lastName());
+		userDetailsRequestObject.put("dateOfBirth", "1990-01-01");
+		userDetailsRequestObject.put("taxNumber", "1234567890");
+		userDetailsRequestObject.put("email", faker.internet().emailAddress());
+		userDetailsRequestObject.put("phoneNumber", faker.phoneNumber().phoneNumber());
+		userDetailsRequestObject.put("address", "123 Main St, Springfield");
+
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+		HttpEntity<String> requestEntity = new HttpEntity<>(userDetailsRequestObject.toString(), httpHeaders);
+
+		ResponseEntity<ClientResponseDTO> createdUserDetailsEntity = testRestTemplate.postForEntity(
+			API_CLIENT_V1 + ADD_CLIENT,
+			requestEntity,
+			ClientResponseDTO.class);
+
+		ClientResponseDTO createdUserDetails = createdUserDetailsEntity.getBody();
+
+		// Act
+		ResponseEntity<ClientResponseDTO> deleatedClientResponse = testRestTemplate.exchange(
+			API_CLIENT_V1 + DELETE_CLIENT,
+			HttpMethod.DELETE,
+			null,
+			ClientResponseDTO.class,
+			createdUserDetails.getUUID());
+
+		createdUserDetails = deleatedClientResponse.getBody();
+
+		// Assert
+		try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
+			softly.assertThat(deleatedClientResponse.getStatusCode())
+				.as("Response status should be CREATED")
+				.isEqualTo(HttpStatus.OK);
 
 			softly.assertThat(createdUserDetails.getUUID())
 				.as("Created user UUID should not be null")
