9eb68e3231f527561f0299f3283ecac506e3707b
ihomziak <ivan.homziak@onedatascan.com>
2025-06-22 14:20:25 +0200

    added integration test testUpdateClient_whenValidDetailsProvided_returnsClientDetails

--- src/test/java/com/ihomziak/clientaccountms/controller/integrationtest/ClientControllerIntegrationTestcontainersTest.java
+++ src/test/java/com/ihomziak/clientaccountms/controller/integrationtest/ClientControllerIntegrationTestcontainersTest.java
@@ -3,6 +3,7 @@ package com.ihomziak.clientaccountms.controller.integrationtest;
 import static com.ihomziak.clientaccountms.util.constants.Endpoints.ClientEndpoints.ADD_CLIENT;
 import static com.ihomziak.clientaccountms.util.constants.Endpoints.ClientEndpoints.API_CLIENT_V1;
 import static com.ihomziak.clientaccountms.util.constants.Endpoints.ClientEndpoints.DELETE_CLIENT;
+import static com.ihomziak.clientaccountms.util.constants.Endpoints.ClientEndpoints.UPDATE_CLIENT;
 
 import java.util.Arrays;
 
@@ -30,6 +31,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.ihomziak.clientaccountms.controller.integrationtest.config.TestSecurityConfig;
+import com.ihomziak.clientaccountms.dto.ClientRequestDTO;
 import com.ihomziak.clientaccountms.dto.ClientResponseDTO;
 
 import net.datafaker.Faker;
@@ -143,6 +145,134 @@ public class ClientControllerIntegrationTestcontainersTest {
 			softly.assertThat(createdUserDetails.getAddress())
 				.as("Address should match")
 				.isEqualTo(userDetailsRequestObject.get("address"));
+		}
+	}
+
+
+	@Test
+	@DisplayName("Created user can be updated")
+	public void testUpdateClient_whenValidDetailsProvided_returnsClientDetails() throws JSONException {
+		// Arrange
+		String taxNumber = faker.phoneNumber().phoneNumber();
+		ClientRequestDTO body = ClientRequestDTO.builder()
+			.firstName(faker.name().firstName())
+			.lastName(faker.name().lastName())
+			.dateOfBirth("1990-01-03")
+			.email(faker.internet().emailAddress())
+			.phoneNumber(faker.phoneNumber().phoneNumber())
+			.taxNumber(taxNumber)
+			.address(faker.address().streetAddress())
+			.build();
+
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+		HttpEntity<ClientRequestDTO> requestEntity = new HttpEntity<>(body, httpHeaders);
+
+		ResponseEntity<ClientResponseDTO> createdUserDetailsEntity = testRestTemplate.postForEntity(
+			API_CLIENT_V1 + ADD_CLIENT,
+			requestEntity,
+			ClientResponseDTO.class);
+
+		ClientResponseDTO createdUserDetails = createdUserDetailsEntity.getBody();
+
+		// Assert
+		try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
+			softly.assertThat(createdUserDetailsEntity.getStatusCode())
+				.as("Response status should be CREATED")
+				.isEqualTo(HttpStatus.CREATED);
+
+			softly.assertThat(createdUserDetails.getUUID())
+				.as("Created user UUID should not be null")
+				.isNotNull();
+
+			softly.assertThat(createdUserDetails.getFirstName())
+				.as("First name should match")
+				.isEqualTo(body.getFirstName());
+
+			softly.assertThat(createdUserDetails.getLastName())
+				.as("Last name should match")
+				.isEqualTo(body.getLastName());
+
+			softly.assertThat(createdUserDetails.getDateOfBirth())
+				.as("Date of birth should match")
+				.isEqualTo(body.getDateOfBirth());
+
+			softly.assertThat(createdUserDetails.getTaxNumber())
+				.as("Tax number should match")
+				.isEqualTo(body.getTaxNumber());
+
+			softly.assertThat(createdUserDetails.getEmail())
+				.as("Email should match")
+				.isEqualTo(body.getEmail());
+
+			softly.assertThat(createdUserDetails.getPhoneNumber())
+				.as("Phone number should match")
+				.isEqualTo(body.getPhoneNumber());
+
+			softly.assertThat(createdUserDetails.getAddress())
+				.as("Address should match")
+				.isEqualTo(body.getAddress());
+		}
+
+		// Arrange
+		body = ClientRequestDTO.builder()
+			.firstName(faker.name().firstName())
+			.lastName(faker.name().lastName())
+			.dateOfBirth("1990-01-03")
+			.email(faker.internet().emailAddress())
+			.phoneNumber(faker.phoneNumber().phoneNumber())
+			.taxNumber(taxNumber)
+			.address(faker.address().streetAddress())
+			.build();
+		requestEntity = new HttpEntity<>(body, httpHeaders);
+		// Act
+		ResponseEntity<ClientResponseDTO> updateClientResponse = testRestTemplate.exchange(
+			API_CLIENT_V1 + UPDATE_CLIENT,
+			HttpMethod.PATCH,
+			requestEntity,
+			ClientResponseDTO.class
+		);
+
+		createdUserDetails = updateClientResponse.getBody();
+		// Assert
+		try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
+			softly.assertThat(updateClientResponse.getStatusCode())
+				.as("Response status should be CREATED")
+				.isEqualTo(HttpStatus.ACCEPTED);
+
+			softly.assertThat(createdUserDetails.getUUID())
+				.as("Created user UUID should not be null")
+				.isNotNull();
+
+			softly.assertThat(createdUserDetails.getFirstName())
+				.as("First name should match")
+				.isEqualTo(body.getFirstName());
+
+			softly.assertThat(createdUserDetails.getLastName())
+				.as("Last name should match")
+				.isEqualTo(body.getLastName());
+
+			softly.assertThat(createdUserDetails.getDateOfBirth())
+				.as("Date of birth should match")
+				.isEqualTo(body.getDateOfBirth());
+
+			softly.assertThat(createdUserDetails.getTaxNumber())
+				.as("Tax number should match")
+				.isEqualTo(body.getTaxNumber());
+
+			softly.assertThat(createdUserDetails.getEmail())
+				.as("Email should match")
+				.isEqualTo(body.getEmail());
+
+			softly.assertThat(createdUserDetails.getPhoneNumber())
+				.as("Phone number should match")
+				.isEqualTo(body.getPhoneNumber());
+
+			softly.assertThat(createdUserDetails.getAddress())
+				.as("Address should match")
+				.isEqualTo(body.getAddress());
 		}
 	}
 
